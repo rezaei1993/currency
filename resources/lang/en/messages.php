1d9abf24faee85d0daf878b9ae7b66805fd1a6d1
rezaei1993 <d.rezaei1993@gmail.com>
2024-02-29 01:21:49 +0330

    feat: added Lang file

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'created_success' => 'Resource created successfully',
+    'not_found' => 'Not Found!',
+    'error' => 'error',
+    'success' => 'success',
+    'fail' => 'fail',
+    'internal_error' => 'Internal error',
+];
