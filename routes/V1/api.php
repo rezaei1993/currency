ac2fc4f107ec155fc2afb035417f4f7221b030a3
rezaei1993 <d.rezaei1993@gmail.com>
2024-02-29 01:15:24 +0330

    feat: added Apis

--- routes/V1/api.php
+++ routes/V1/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\V1\CurrencyAssetController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('currency/assets')->name('currency.assets.')->group(function () {
+    Route::get('/', [CurrencyAssetController::class, 'list'])->name('list');
+    Route::post('/', [CurrencyAssetController::class, 'add'])->name('add');
+    Route::get('/calculate', [CurrencyAssetController::class, 'calculate'])->name('calculate');
 });
+
+
+
+
